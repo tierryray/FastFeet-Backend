29ec81bb5de6ab4f22da1735505c4951d53c3c89
Tierry Ray <tierry.machado@agilecontent.com>
2020-02-22 17:35:32 -0300

    DeliveryMan and Files

---
+++ src/app/controllers/DeliveryManController.js
@@ -0,0 +1,128 @@
+import * as Yup from 'yup';
+import DeliveryMan from '../models/DeliveryMan';
+import File from '../models/File';
+
+class DeliveryManController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      avatar_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { avatar_id, email } = req.body;
+
+    const file = await File.findOne({ where: { id: avatar_id } });
+
+    if (!file) {
+      return res.status(400).json({ error: 'File not found!' });
+    }
+
+    const userExists = await DeliveryMan.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const deliveryMan = await DeliveryMan.create(req.body);
+
+    return res.json(deliveryMan);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      avatar_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await DeliveryMan.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman not found' });
+    }
+
+    const { email } = req.body;
+
+    if (email && email !== deliveryman.email) {
+      const userExists = await DeliveryMan.findOne({ where: { email } });
+
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ error: 'The email provided is already being used' });
+      }
+    }
+
+    const { avatar_id } = req.body;
+
+    if (avatar_id) {
+      const file = await File.findOne({ where: { id: avatar_id } });
+
+      if (!file) {
+        return res.status(400).json({ error: 'Avatar file not found!' });
+      }
+    }
+
+    const { id, name } = await deliveryman.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar_id,
+    });
+  }
+
+  async index(req, res) {
+    const { id } = req.params;
+
+    if (id) {
+      const deliveryman = await DeliveryMan.findOne({ where: { id } });
+
+      if (!deliveryman) {
+        return res.status(400).json({ error: 'Deliveryman not found' });
+      }
+
+      return res.json(deliveryman);
+    }
+
+    const deliverymans = await DeliveryMan.findAll();
+
+    if (!deliverymans) {
+      return res.status(400).json({ error: 'Deliverymans not founded' });
+    }
+
+    return res.json(deliverymans);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const deliveryman = await DeliveryMan.findByPk(id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman not exists' });
+    }
+
+    await deliveryman.destroy();
+
+    const deliverymans = await DeliveryMan.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+    });
+
+    return res.json(deliverymans);
+  }
+}
+
+export default new DeliveryManController();
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,33 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import DeliveryManController from './app/controllers/DeliveryManController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/login', SessionController.store);
 
 routes.use(authMiddleware);
-routes.put('/users', UserController.update);
+
+routes.post('/users', UserController.store);
+routes.put('/user', UserController.update);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/deliverymans/:id?', DeliveryManController.index);
+routes.post('/deliveryman', DeliveryManController.store);
+routes.put('/deliveryman/:id', DeliveryManController.update);
+routes.delete('/deliveryman/:id', DeliveryManController.delete);
 
 export default routes;
